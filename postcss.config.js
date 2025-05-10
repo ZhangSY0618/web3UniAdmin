61da2e7337026ade826fcf25ebefe13dfa6051a9
zhang <185355787@qq.com>
2025-05-10 16:53:49 +0800

    ''

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('@tailwindcss/postcss'), // Use the correct Tailwind CSS PostCSS plugin
-    // require('autoprefixer'),
+    require('autoprefixer'),
     require('postcss-preset-env')({
       stage: 2,
       // browsers: 'last 2 versions, > 1%',
--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    {/* <div className='test'>test</div>
-    <div className="card">
-      <h1 className="title">Hello, Tailwind!</h1>
-      <button className="button">Click Me</button>
-    </div> */}
     <App />
   </React.StrictMode>
 );
